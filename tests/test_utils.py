5ad98e8a620d6cded26e42f6fe45b835da9d78c2
David G Rosenberg <targendaz2@me.com>
2019-10-02 00:05:26 -0400

    Refactored plist seeding funcs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,12 +2,13 @@ import os, shutil
 
 from test_settings import *
 
+def seed_plist(plist_name, plist, tmp_dir):
+	seeded_plist = os.path.join(tmp_dir, plist_name)
+	shutil.copy(plist, seeded_plist)
+	return seeded_plist
+
 def seed_simple_binary_plist(tmp_dir):
-	plist = os.path.join(tmp_dir, SIMPLE_BINARY_PLIST_NAME)
-	shutil.copy(SIMPLE_BINARY_PLIST, plist)
-	return plist
+	return seed_plist(SIMPLE_BINARY_PLIST_NAME, SIMPLE_BINARY_PLIST, tmp_dir)
 
 def seed_xml_plist(tmp_dir):
-	plist = os.path.join(tmp_dir, XML_PLIST_NAME)
-	shutil.copy(XML_PLIST, plist)
-	return plist
+	return seed_plist(XML_PLIST_NAME, XML_PLIST, tmp_dir)
