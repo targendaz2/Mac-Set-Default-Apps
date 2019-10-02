2693124a92fe8866ff77eb704d6b16eddacac671
David G Rosenberg <targendaz2@me.com>
2019-10-01 22:40:02 -0400

    Removed is_uti method

--- tests/tests.py
+++ tests/tests.py
@@ -14,23 +14,6 @@ msda = imp.load_source('msda', os.path.join(
 	THIS_FILE, '../payload/msda')
 )
 
-class TestStringProcessing(unittest.TestCase):
-
-	def test_can_identify_standard_UTIs(self):
-		test_string = 'com.company.fakeapp'
-		result = msda.is_uti(test_string)
-		self.assertTrue(result)
-
-	def test_can_identify_longer_UTIs(self):
-		test_string = 'com.company.fakeapp.pro'
-		result = msda.is_uti(test_string)
-		self.assertTrue(result)
-
-	def test_can_identify_non_UTIs(self):
-		test_string = 'http'
-		result = msda.is_uti(test_string)
-		self.assertFalse(result)
-
 
 class TestPListReadAndWrite(unittest.TestCase):
 
