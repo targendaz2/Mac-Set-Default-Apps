589c5c0c0151d1b29d3ae1bf1a6ce0c2d0bb0805
David Rosenberg <targendaz2@me.com>
2019-10-21 23:20:56 -0400

    Create testing dirs in project dir

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,7 +2,7 @@ import os, sys
 
 THIS_FILE = os.path.dirname(sys.argv[0])
 
-TMP_PREFIX = 'msda_tmp_'
+TMP_ROOT = os.path.join(THIS_FILE, '..', 'tmp')
 SIMPLE_BINARY_PLIST = 'ex_simple_binary.plist'
 BINARY_PLIST = 'ex_binary.plist'
 XML_PLIST = 'ex_xml.plist'
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,10 +7,10 @@ from unittest import TestCase
 from faker import Faker
 from faker.providers import file, internet, lorem
 
-from test_settings import THIS_FILE, TMP_PREFIX
+from test_settings import *
 
 msda = imp.load_source('msda', os.path.join(
-	THIS_FILE, '../payload/msda')
+    THIS_FILE, '../payload/msda')
 )
 
 fake = Faker()
@@ -20,126 +20,129 @@ fake.add_provider(lorem)
 
 # Faker Functions
 def fake_app_id():
-	app_id = '{}.{}.{}'.format(
-		fake.tld(),
-		fake.domain_word(),
-		fake.word(),
-	)
-	if random() > 0.1:
-		return app_id 					# 90% chance: com.company.app
-	return app_id + '.' + fake.word()	# 10% chance: com.company.app.pro
+    app_id = '{}.{}.{}'.format(
+        fake.tld(),
+        fake.domain_word(),
+        fake.word(),
+    )
+    if random() > 0.1:
+        return app_id 					# 90% chance: com.company.app
+    return app_id + '.' + fake.word()	# 10% chance: com.company.app.pro
 
 def fake_uti():
-	if random() > 0.25:
-		return 'public.{}'.format(		# 75% chance: public.html
-			fake.file_extension()
-		)
-	uti = '{}.{}.{}'.format(
-		fake.tld(),
-		fake.domain_word(),
-		fake.file_extension(),
-	)
-	if random() > 0.8:
-		return uti						# 20% chance: com.company.file
-	return '{}.{}.{}.{}'.format(		#  5% chance: com.company.app.file
-		fake.tld(),
-		fake.domain_word(),
-		fake.word(),
-		fake.file_extension(),
-	)
+    if random() > 0.25:
+        return 'public.{}'.format(		# 75% chance: public.html
+            fake.file_extension()
+        )
+    uti = '{}.{}.{}'.format(
+        fake.tld(),
+        fake.domain_word(),
+        fake.file_extension(),
+    )
+    if random() > 0.8:
+        return uti						# 20% chance: com.company.file
+    return '{}.{}.{}.{}'.format(		#  5% chance: com.company.app.file
+        fake.tld(),
+        fake.domain_word(),
+        fake.word(),
+        fake.file_extension(),
+    )
 
 def fake_protocol():
-	protocols = [
-		'http',
-		'https',
-		'xml',
-		'xhtml',
-		'ftp',
-	]
-	return choice(protocols)
+    protocols = [
+        'http',
+        'https',
+        'xml',
+        'xhtml',
+        'ftp',
+    ]
+    return choice(protocols)
 
 def fake_role(all=False):
-	roles = ['editor', 'viewer']
-	if all:
-		roles.append('all')
-	return choice(roles)
+    roles = ['editor', 'viewer']
+    if all:
+        roles.append('all')
+    return choice(roles)
 
 # LSHandler Factories
 def lshandler_factory(num=1, all=True, uti=False, protocol=False):
-	app_id = fake_app_id()
-	handlers = []
-	utis = []
-	for n in range(num):
-		rand_num = random()
-		if (rand_num > 0.5 and not uti) or uti:
-			uti = fake_uti()
-			role = fake_role(all=all)
-			handler = msda.LSHandler(
-				app_id=app_id,
-				uti=uti,
-				role=role
-			)
-		if (rand_num <= 0.5 and not protocol) or protocol:
-			uti = fake_protocol()
-			handler = msda.LSHandler(
-				app_id=app_id,
-				uti=uti,
-			)
-		handlers.append(handler)
-	return handlers
+    app_id = fake_app_id()
+    handlers = []
+    utis = []
+    for n in range(num):
+        rand_num = random()
+        if (rand_num > 0.5 and not uti) or uti:
+            uti = fake_uti()
+            role = fake_role(all=all)
+            handler = msda.LSHandler(
+                app_id=app_id,
+                uti=uti,
+                role=role
+            )
+        if (rand_num <= 0.5 and not protocol) or protocol:
+            uti = fake_protocol()
+            handler = msda.LSHandler(
+                app_id=app_id,
+                uti=uti,
+            )
+        handlers.append(handler)
+    return handlers
 
 # Sample LSHandlers
 html_viewer_lshandler = msda.LSHandler(
-	app_id=fake_app_id(),
-	uti=fake_uti(),
-	role=fake_role(),
+    app_id=fake_app_id(),
+    uti=fake_uti(),
+    role=fake_role(),
 )
 
 https_lshandler = msda.LSHandler(
-	app_id=fake_app_id(),
-	uti=fake_protocol(),
+    app_id=fake_app_id(),
+    uti=fake_protocol(),
 )
 
 url_all_lshandler = msda.LSHandler(
-	app_id=fake_app_id(),
-	uti=fake_uti(),
+    app_id=fake_app_id(),
+    uti=fake_uti(),
 )
 
 
 # Sample LSHandler Dicts
 def protocol_lshandler_dict(app_id, uti):
-	role_key = 'LSHandlerRoleAll'
-	dict_ = {
-		'LSHandlerURLScheme': uti.lower(),
-		role_key: app_id,
-		'LSHandlerPreferredVersions': {
-			role_key: '-',
-		}
-	}
-	return dict_
+    role_key = 'LSHandlerRoleAll'
+    dict_ = {
+        'LSHandlerURLScheme': uti.lower(),
+        role_key: app_id,
+        'LSHandlerPreferredVersions': {
+            role_key: '-',
+        }
+    }
+    return dict_
 
 def uti_lshandler_dict(app_id, uti, role='all'):
-	role_key = 'LSHandlerRole' + role.capitalize()
-	dict_ = {
-		'LSHandlerContentType': uti,
-		role_key: app_id,
-		'LSHandlerPreferredVersions': {
-			role_key: '-',
-		}
-	}
-	return dict_
+    role_key = 'LSHandlerRole' + role.capitalize()
+    dict_ = {
+        'LSHandlerContentType': uti,
+        role_key: app_id,
+        'LSHandlerPreferredVersions': {
+            role_key: '-',
+        }
+    }
+    return dict_
 
 # Abstract Classes
 class LaunchServicesTestCase(TestCase):
 
-	def setUp(self):
-		self.tmp = tempfile.mkdtemp(prefix=TMP_PREFIX)
+    def setUp(self):
+        os.mkdir(TMP_ROOT)
+        self.tmp = tempfile.mkdtemp(
+            dir=os.path.join(THIS_FILE, '..', 'tmp'),
+        )
 
-	def tearDown(self):
-		shutil.rmtree(self.tmp)
+    def tearDown(self):
+        shutil.rmtree(TMP_ROOT)
 
-	def seed_plist(self, plist_name):
-		src = os.path.join(THIS_FILE, 'assets', plist_name)
-		dest = os.path.join(self.tmp, plist_name)
-		shutil.copy(src, dest)
-		return dest
+    def seed_plist(self, plist_name):
+        src = os.path.join(THIS_FILE, 'assets', plist_name)
+        dest = os.path.join(self.tmp, plist_name)
+        shutil.copy(src, dest)
+        return dest
