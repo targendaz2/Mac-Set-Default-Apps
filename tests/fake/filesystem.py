8b50beb09d49d9e0462de22289288fa76a1d16a2
David G Rosenberg <targendaz2@me.com>
2019-12-27 09:06:08 -0500

    Fake fs is deleted between tests

--- tests/fake/filesystem.py
+++ tests/fake/filesystem.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import imp
 import os
+import shutil
 import tempfile
 
 from faker import Faker
@@ -10,17 +11,25 @@ from faker.providers import internet
 fake = Faker()
 fake.add_provider(internet)
 
-FAKE_FS_ROOT = tempfile.mkdtemp()
-USER_HOMES_DIR = 'Users'
+TMP_PREFIX = 'fake_fs_'
 
-def create_user_homes(number=1):
-	created_user_homes = []
-	for n in range(number):
-		user_home_path = os.path.join(
-			FAKE_FS_ROOT,
-			USER_HOMES_DIR,
-			fake.user_name(),
-		)
-		os.makedirs(user_home_path)
-		created_user_homes.append(user_home_path)
-	return created_user_homes
+class FakeFileSystem(object):
+
+	def __init__(self):
+		self.ROOT_DIR = tempfile.mkdtemp(prefix=TMP_PREFIX)
+		self.USER_HOMES_DIR = 'Users'
+
+	def __del__(self):
+		shutil.rmtree(self.ROOT_DIR)
+
+	def create_user_homes(self, number=1):
+		created_user_homes = []
+		for n in range(number):
+			user_home_path = os.path.join(
+				self.ROOT_DIR,
+				self.USER_HOMES_DIR,
+				fake.user_name(),
+			)
+			os.makedirs(user_home_path)
+			created_user_homes.append(user_home_path)
+		return created_user_homes
--- tests/fake_filesystem_tests.py
+++ tests/fake_filesystem_tests.py
@@ -11,26 +11,31 @@ from random import randint
 
 import mock
 
-from fake import filesystem as fs
+from fake.filesystem import *
 from utils.settings import *
 
 msda = imp.load_source('msda', os.path.join(
 	THIS_FILE, '../payload/msda.py')
 )
 
-
 class TestFakeFileSystemFunctions(TestCase):
 
+	def setUp(self):
+		self.fs = FakeFileSystem()
+
 	def test_can_create_single_user_home(self):
-		user_homes = fs.create_user_homes(1)
+		user_homes = self.fs.create_user_homes(1)
 		self.assertTrue(os.path.exists(user_homes[0]))
 
 	def test_can_create_multiple_user_homes(self):
 		num_homes = randint(2, 10)
-		user_homes = fs.create_user_homes(num_homes)
+		user_homes = self.fs.create_user_homes(num_homes)
 		self.assertEqual(num_homes, len(user_homes))
 		for user_home in user_homes:
 			self.assertTrue(os.path.exists(user_home))
+
+	def test_contents_dont_persist_between_tests(self):
+		self.assertFalse(os.listdir(self.fs.ROOT_DIR))
 
 
 if __name__ == '__main__':
