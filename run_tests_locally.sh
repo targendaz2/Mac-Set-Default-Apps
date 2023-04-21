01c269ef32edb5ba9eb6eeff9ec12f577722d4f1
David G Rosenberg <targendaz2@me.com>
2023-04-21 21:19:17 +0800

    Removed unneeded switch

--- run_tests_locally.sh
+++ run_tests_locally.sh
@@ -4,7 +4,7 @@ mkdir -p ./.test_results
 
 results_filename="$(date '+results_%Y%m%d%H%M%S.html')"
 
-cirrus run --artifacts-dir .test_results --environment RESULTS_FILENAME=$results_filename --output interactive
+cirrus run --artifacts-dir .test_results --environment RESULTS_FILENAME=$results_filename
 
 open ./.test_results/run_tests/results/$results_filename
 
