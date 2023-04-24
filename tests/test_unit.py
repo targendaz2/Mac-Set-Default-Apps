2cea0188ea1d43bca9c651af3720ebe68198380e
David G Rosenberg <targendaz2@me.com>
2023-04-24 21:02:34 +0800

    Switched initial test to Safari from Chrome

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -4,17 +4,17 @@ class TestUnit:
 
     def test_can_find_app_url_from_app_id_if_installed(self):
         # Given the ID of an installed app
-        app_id = 'com.google.Chrome'
+        app_id = 'com.apple.Safari'
 
         # When that app ID is submitted
         app_url = _get_app_url(app_id)
 
         # Then the app's URL should be returned
-        assert app_url == '/Applications/Google Chrome.app'
+        assert app_url == '/Applications/Safari.app'
 
     def test_cant_find_app_url_from_app_id_if_not_installed(self):
         # Given the ID of an app that isn't installed
-        app_id = 'com.microsoft.edgemac'
+        app_id = 'com.dgrdev.fakebrowser'
 
         # When that app ID is submitted
         app_url = _get_app_url(app_id)
