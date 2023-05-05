5f88eb8fd09229f462adab4cfd197d100d1049f9
David G Rosenberg <targendaz2@me.com>
2023-05-06 03:45:13 +0800

    Started working on arg parsing

--- src/msda.sh
+++ src/msda.sh
@@ -1,11 +1,39 @@
 #!/usr/bin/env zsh
 
-FIRST_RUN_FILE=/tmp/bats-tutorial-project-ran
+function print_help() {
+    local message="$1"
+    [ ! -z "$message" ] && print "$message\n"
+    print -rC1 --      \
+        "msda.sh [-h|--help]" \
+        "msda.sh [set]"
+    exit
+}
 
-if [[ ! -e "$FIRST_RUN_FILE" ]]; then
-    echo "Welcome to our project!"
-    touch "$FIRST_RUN_FILE"
+function set_command() {
+    print command is set
+    exit
+}
+
+# Parse keyword args
+zparseopts -D -E -F -- \
+    {h,-help}=help \
+    || exit
+
+# Get positional args
+command=$1
+
+# Parse positional args
+case $command in
+    set)
+        set_command
+        ;;
+    *)
+        print_help "\"$command\" is not a valid command"
+        ;;
+esac
+
+
+if (( $#help )); then
+    print_help
+    return
 fi
-
-echo "NOT IMPLEMENTED!" >&2
-exit 1
