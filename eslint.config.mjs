1c568c8d1623e8b3663a5fd2fb033acf2a409620
David G Rosenberg <targendaz2@me.com>
2024-04-11 17:35:08 -0400

    Switched to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+);
