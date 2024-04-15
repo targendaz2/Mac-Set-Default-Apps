2777356e2526802fa99c3ae8e08d356c8c28e362
David G Rosenberg <targendaz2@me.com>
2024-04-15 16:30:15 -0400

    Fixed git capitalization issue

---
+++ src/models/UTI.ts
@@ -0,0 +1,33 @@
+import type Config from '@/src/config.json';
+import '@jxa/global-type';
+
+const config: typeof Config = require('@/src/config.json');
+
+export class UTI {
+    readonly id: string;
+    readonly tags: string[];
+
+    constructor(id: string) {
+        const app = Application.currentApplication();
+        app.includeStandardAdditions = true;
+
+        this.id = id;
+
+        // This long awk command essentially parses the lsregister dump for
+        // ID's and tags matching the given UTI. The lsregister dump is huge
+        // and the command doesn't natively allow searching or filtering.
+        // TODO: Handle the lsregister dump parsing in JavaScript
+        const cmd = `${config.systemPaths.lsregister} -gc -dump Type | awk -F ':' "{ if (\\$1 == \\"type id\\" && \\$2 ~ \\"public.html\\") { check=\\"yes\\" } else if (\\$1 == \\"type id\\") { check=\\"no\\" } else if (\\$1 == \\"tags\\" && check == \\"yes\\") { print \\$2 } }"`;
+
+        const result = app.doShellScript(cmd);
+
+        this.tags = Array.from(
+            new Set(
+                result
+                    .replace(/["'][\w ]+["'],?/g, '')
+                    .replace(/\r|\.| /g, '')
+                    .split(','),
+            ),
+        );
+    }
+}
