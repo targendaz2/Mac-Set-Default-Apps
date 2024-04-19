814b52484ace2f2660d45eb32363773046c08a13
David G Rosenberg <targendaz2@me.com>
2024-04-19 06:10:20 -0400

    Arg parser can accept positional param for app bundle ID

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import type Config from '@/src/config.json';
-import type { App as AppClass, UTI as UTIClass } from '@/src/models';
+import { parseArgs } from '@/src/argparse';
 
-const config: typeof Config = require('@/src/config.json');
-const App: typeof AppClass = require('@/src/models').App;
-const UTI: typeof UTIClass = require('@/src/models').UTI;
+// import type Config from '@/src/config.json';
+// import type { App as AppClass, UTI as UTIClass } from '@/src/models';
 
-const app = new App('com.apple.Safari');
-console.log(app.name + ' as default browser');
+// const config: typeof Config = require('@/src/config.json');
+// const App: typeof AppClass = require('@/src/models').App;
+// const UTI: typeof UTIClass = require('@/src/models').UTI;
 
-for (const [utiID, role] of Object.entries(
-    config.defaultAppRequirements.browser.utis,
-)) {
-    const uti = new UTI(utiID);
-    // @ts-expect-error 'role will return one of the expected values'
-    console.log(`Supports ${utiID}: ` + app.supportsUTI(uti, role));
+// const app = new App('com.apple.Safari');
+// console.log(app.name + ' as default browser');
+
+// for (const [utiID, role] of Object.entries(
+//     config.defaultAppRequirements.browser.utis,
+// )) {
+//     const uti = new UTI(utiID);
+//     // @ts-expect-error 'role will return one of the expected values'
+//     console.log(`Supports ${utiID}: ` + app.supportsUTI(uti, role));
+// }
+
+// for (const urlScheme of config.defaultAppRequirements.browser.urlSchemes) {
+//     console.log(`Supports ${urlScheme}: ` + app.supportsURLScheme(urlScheme));
+// }
+
+function main() {
+    console.log(parseArgs(['com.apple.Safari', '--browser']));
 }
 
-for (const urlScheme of config.defaultAppRequirements.browser.urlSchemes) {
-    console.log(`Supports ${urlScheme}: ` + app.supportsURLScheme(urlScheme));
-}
+main();
---
+++ tests/unit/argparse.test.ts
@@ -0,0 +1,47 @@
+// import cp from 'node:child_process';
+import cp from 'node:child_process';
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
+import { getArgs, parseArgs } from '@/src/argparse';
+import { run } from '@/tests/helpers/jxaRun';
+import { describe, expect, test } from '@jest/globals';
+
+describe('argument fetching tests', () => {
+    test('can get command line arguments', async () => {
+        const scriptFile = path.join(fs.mkdtempSync(os.tmpdir()), 'script.js');
+        fs.writeFileSync(scriptFile, `${getArgs.toString()}\ngetArgs();`);
+
+        const result = cp
+            .execFileSync(
+                'osascript',
+                [
+                    '-l',
+                    'JavaScript',
+                    scriptFile,
+                    'com.apple.Safari',
+                    '--browser',
+                    '--feu',
+                ],
+                {
+                    encoding: 'utf8',
+                },
+            )
+            .trim()
+            .split(', ');
+
+        expect(result).toContain('com.apple.Safari');
+        expect(result).toContain('--browser');
+        expect(result).toContain('--feu');
+    });
+});
+
+describe('argument parsing tests', () => {
+    test('accepts positional parameter for an app bundle ID', async () => {
+        const result = await run<string>((fn: typeof parseArgs) => {
+            return fn(['com.apple.Safari']);
+        }, parseArgs);
+
+        expect(result).toBe('com.apple.Safari');
+    });
+});
