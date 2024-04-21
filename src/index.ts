397596212286208e9f068efa56ee90ac114c8745
David G Rosenberg <targendaz2@me.com>
2024-04-20 21:54:16 -0400

    config.json schema is generated from TypeScript interface

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import { parseArgs } from '@/src/argparse';
-
-// import type Config from '@/src/config.json';
-// import type { App as AppClass, UTI as UTIClass } from '@/src/models';
-
-// const config: typeof Config = require('@/src/config.json');
-// const App: typeof AppClass = require('@/src/models').App;
-// const UTI: typeof UTIClass = require('@/src/models').UTI;
-
-// const app = new App('com.apple.Safari');
-// console.log(app.name + ' as default browser');
-
-// for (const [utiID, role] of Object.entries(
-//     config.defaultAppRequirements.browser.utis,
-// )) {
-//     const uti = new UTI(utiID);
-//     // @ts-expect-error 'role will return one of the expected values'
-//     console.log(`Supports ${utiID}: ` + app.supportsUTI(uti, role));
-// }
-
-// for (const urlScheme of config.defaultAppRequirements.browser.urlSchemes) {
-//     console.log(`Supports ${urlScheme}: ` + app.supportsURLScheme(urlScheme));
-// }
+import { arg } from '@/src/args';
 
 function main() {
-    console.log(parseArgs(['com.apple.Safari', '--browser']));
+    const args = arg({
+        '--help': Boolean,
+        '--browser': Boolean,
+    });
+    console.log(args['_']);
+    console.log(args['--browser']);
 }
 
 main();
--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,48 @@ export type InfoPlist = {
 export type JXAApplication = typeof Application &
     Application._StandardAdditions &
     Application.AnyValue;
+
+export type UTIRole = 'Viewer' | 'Shell' | 'QLGenerator' | 'None' | 'All';
+
+type DefaultApp = {
+    /**
+     * UTI's required by this default app definition.
+     *
+     * @minProperties 1
+     * */
+    utis: {
+        [key: string]: UTIRole;
+    };
+
+    /**
+     * URL schemes by this default app definition.
+     *
+     * @minItems 1
+     * */
+    urlSchemes: string[];
+};
+
+export interface Config {
+    $schema: string;
+
+    /**
+     * Requirements an app must meet to be set as a default.
+     *
+     * @minProperties 1
+     * */
+    defaultAppRequirements: {
+        [key: string]:
+            | Pick<DefaultApp, 'utis'>
+            | Pick<DefaultApp, 'urlSchemes'>
+            | DefaultApp;
+    };
+
+    /**
+     * Paths to system files and directories.
+     *
+     * @minProperties 1
+     * */
+    systemPaths: {
+        [key: string]: string;
+    };
+}
