c099ab2c122fcbefefe39bc580bdcfba33a7a4a2
David G Rosenberg <targendaz2@me.com>
2024-04-20 21:52:27 -0400

    Added test to check for zsh

--- tests/unit/shell.test.ts
+++ tests/unit/shell.test.ts
@@ -1,11 +1,14 @@
 import '@/tests/extensions/fs.ext';
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 
-describe('lsregister tests', () => {
-    test('command exists', async () => {
-        const path =
-            '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister';
+test('lsregister command exists', () => {
+    const path =
+        '/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister';
 
-        expect(path).toExist();
-    });
+    expect(path).toExist();
+});
+
+test('zsh command exists', () => {
+    const path = '/bin/zsh';
+    expect(path).toExist();
 });
