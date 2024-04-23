eb9e00440e45dd657af6111739ebf9e5281dc216
David G Rosenberg <targendaz2@me.com>
2024-04-22 22:32:18 -0400

    Created LaunchServices model

--- src/models/LaunchServices.ts
+++ src/models/LaunchServices.ts
@@ -1,1 +1,22 @@
-export class LaunchServices {}
+import type { LSHandler, LaunchServicesPlist } from '@/src/types';
+import type { JXAApplication } from '@/src/types';
+import '@jxa/global-type';
+
+export class LaunchServices {
+    readonly lsHandlers: LSHandler[] = [];
+
+    constructor(readonly plist: string) {
+        const app: JXAApplication = Application.currentApplication();
+        app.includeStandardAdditions = true;
+
+        const path = Path(this.plist);
+
+        if (app.exists(path)) {
+            const contents: LaunchServicesPlist = ObjC.deepUnwrap(
+                $.NSDictionary.dictionaryWithContentsOfFile(this.plist),
+            );
+
+            this.lsHandlers = contents.LSHandlers;
+        }
+    }
+}
