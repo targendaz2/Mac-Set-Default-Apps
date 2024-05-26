9ee9ddbe330b862553e9534e742e69911064bd72
David G Rosenberg <targendaz2@me.com>
2024-05-25 22:21:52 -0400

    Can deserialize Document Types

--- src/tests/unit/macos/launchServices/serialization.test.ts
+++ src/tests/unit/macos/launchServices/serialization.test.ts
@@ -1,8 +1,11 @@
-import { documentTypeSchemaFactory } from '@/factories';
+import { documentTypeFactory, documentTypeSchemaFactory } from '@/factories';
 import { run } from '@/lib/jxaRun';
 import { DocumentType } from '@/lib/macos/launchServices/interfaces';
 import { DocumentTypeSchema } from '@/lib/macos/launchServices/schemas';
-import { serializeDocumentType } from '@/lib/macos/launchServices/serializers';
+import {
+    deserializeDocumentType,
+    serializeDocumentType,
+} from '@/lib/macos/launchServices/serializers';
 import { describe, expect, test } from '@jest/globals';
 
 describe('Document Type serialization', () => {
@@ -11,10 +14,10 @@ describe('Document Type serialization', () => {
 
         const result = await run<DocumentType>(
             (
-                schema: DocumentTypeSchema,
+                documentTypeSchema: DocumentTypeSchema,
                 serialize: typeof serializeDocumentType,
             ) => {
-                return serialize(schema);
+                return serialize(documentTypeSchema);
             },
             documentTypeSchema,
             serializeDocumentType,
@@ -38,13 +41,66 @@ describe('Document Type serialization', () => {
         expect(async () => {
             await run<DocumentType>(
                 (
-                    schema: DocumentTypeSchema,
+                    documentTypeSchema: DocumentTypeSchema,
                     serialize: typeof serializeDocumentType,
                 ) => {
-                    return serialize(schema);
+                    return serialize(documentTypeSchema);
                 },
                 documentTypeSchema,
                 serializeDocumentType,
+            );
+        }).rejects.toThrowError();
+    });
+
+    test('can deserialize a valid Document Type', async () => {
+        const documentType = documentTypeFactory.build();
+
+        const result = await run<DocumentTypeSchema>(
+            (
+                documentType: DocumentType,
+                deserialize: typeof deserializeDocumentType,
+            ) => {
+                return deserialize(documentType);
+            },
+            documentType,
+            deserializeDocumentType,
+        );
+
+        [
+            'LSHandlerContentType',
+            'LSHandlerPreferredVersions',
+            `LSHandlerRole${documentType.role}`,
+        ].forEach((prop) => {
+            expect(result).toHaveProperty(prop);
+        });
+
+        expect(
+            // @ts-expect-error "DocumentTypeSchema doesn't support variable keys"
+            result['LSHandlerPreferredVersions'][
+                `LSHandlerRole${documentType.role}`
+            ],
+        ).toBe(documentType.appVersion);
+    });
+
+    test("can't deserialize an invalid Document Type", async () => {
+        const documentType = {
+            uti: 'public.html',
+            role: 'FakeRole',
+            appID: 'com.apple.Safari',
+            appVersion: '14.0.0.23',
+            type: 'FakeType',
+        };
+
+        expect(async () => {
+            await run<DocumentType>(
+                (
+                    documentType: DocumentType,
+                    deserialize: typeof deserializeDocumentType,
+                ) => {
+                    return deserialize(documentType);
+                },
+                documentType,
+                deserializeDocumentType,
             );
         }).rejects.toThrowError();
     });
