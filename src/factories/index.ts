51410cacd2b73b428fbf21f19abd79334a971977
David G Rosenberg <targendaz2@me.com>
2025-01-12 23:45:16 -0500

    Removed more outdated code

--- src/factories/index.ts
+++
@@ -1,4 +0,0 @@
-export { documentTypeFactory, documentTypeSchemaFactory } from './documentType';
-export { launchServicesSchemaFactory } from './launchServices';
-export { lsHandlerFactory, lsHandlerSchemaFactory } from './lsHandler';
-export { urlSchemeFactory, urlSchemeSchemaFactory } from './urlScheme';
